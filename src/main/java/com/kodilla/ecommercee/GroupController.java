90de8353793b947e0b075ba5b881f7e31430ca6c
IngaPanczyk <ingawaaa@gmail.com>
2020-10-21 23:05:36 +0200

    aktualizacja GropuControllera, dodanie lombooka i utworzenie klasy GroupDto

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,34 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
+    public GroupDto getGroup(Long gropuId) {
+        return new GroupDto((long) 1, "test");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup")
+    public void createGroup(GroupDto groupDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return new GroupDto((long) 2, "updated");
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/GroupDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupDto {
+    Long id;
+    String groupName;
+}
