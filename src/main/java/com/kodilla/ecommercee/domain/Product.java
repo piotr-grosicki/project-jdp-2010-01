7c5622a3fc3606e4f56a46946c93ac948ef4c333
dud3k88 <mateusz.dudek88@gmail.com>
2020-10-24 11:47:25 +0200

    Encja Product - implementacja JDP201001-12

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -2,12 +2,21 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "product")
 
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "productName")
     private String productName;
 
 }
