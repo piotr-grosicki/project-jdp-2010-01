37cb7037555299773ed51d1857dc2a3da32cdd64
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-10-27 20:43:07 +0100

    WIP Created entity and 1:1 with Cart

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,17 +1,86 @@
 package com.kodilla.ecommercee.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
-@Getter
-@AllArgsConstructor
+@Entity
+@Table(name = "USER")
 public class User {
 
-    private Long userId;
+    private Long id;
     private String userName;
     private String userKey;
     private String password;
     private boolean status;
+    private Cart cart;
 
+    public User(Long id, String userName, String userKey, String password, boolean status) {
+        this.id = id;
+        this.userName = userName;
+        this.userKey = userKey;
+        this.password = password;
+        this.status = status;
+    }
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "USER_NAME")
+    public String getUserName() {
+        return userName;
+    }
+
+    @Column(name = "KEY")
+    public String getUserKey() {
+        return userKey;
+    }
+
+    @Column(name = "PASSWORD")
+    public String getPassword() {
+        return password;
+    }
+
+    @Column(name = "STATUS")
+    public boolean isStatus() {
+        return status;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "CART")
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserKey(String userKey) {
+        this.userKey = userKey;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
---
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.domain.UserDto;
+
+public class UserMapper {
+
+    public User mapUserDtoToUser(UserDto userDto) {
+        return new User(
+                userDto.getId(),
+                userDto.getUserName(),
+                userDto.getPassword(),
+                userDto.getUserKey(),
+                userDto.isStatus()
+        );
+    }
+
+    public UserDto mapUserToUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getUserName(),
+                user.getPassword(),
+                user.getUserKey(),
+                user.isStatus()
+        );
+    }
+}
