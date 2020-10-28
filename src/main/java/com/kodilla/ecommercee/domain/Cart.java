46eeefa1734204dd27e40dc65dbc1efe8a48cfdd
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-10-28 14:42:34 +0100

    adding Cart class

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,14 +1,56 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
+@Entity
+@Table(name = "CARTS")
 public class Cart {
+
     private Long Id;
-    private Long userId;
+    private User user;
     private List<Product> productsAddedToCart;
+    private Order order;
+
+    public Cart(Long id, User user, List<Product> productsAddedToCart) {
+        Id = id;
+        this.user = user;
+        this.productsAddedToCart = productsAddedToCart;
+    }
+
+    public Cart() {
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", unique = true)
+    public Long getId() {
+        return Id;
+    }
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    public User getUser() {
+        return user;
+    }
+
+
+    private void setOrder(Order order) {
+        this.order = order;
+    }
+
+    private void setId(Long id) {
+        Id = id;
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
+    private void setProductsAddedToCart(List<Product> productsAddedToCart) {
+        this.productsAddedToCart = productsAddedToCart;
+    }
 }
