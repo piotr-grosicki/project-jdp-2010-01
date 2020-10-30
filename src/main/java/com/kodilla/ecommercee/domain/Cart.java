2debfb5b67b624cae99de2cfde4a490e8ea4dd1d
andzia0520 <ankadaniluk@gmail.com>
2020-10-30 17:07:52 +0100

    JDP201001-14

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -2,19 +2,20 @@ package com.kodilla.ecommercee.domain;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "CARTS")
 public class Cart {
 
-    private Long Id;
+    private Long id;
     private User user;
-    private List<Product> productsAddedToCart;
+    private List<Product> productsAddedToCart = new ArrayList<>();
     private Order order;
 
     public Cart(Long id, User user, List<Product> productsAddedToCart) {
-        this.Id = id;
+        this.id = id;
         this.user = user;
         this.productsAddedToCart = productsAddedToCart;
     }
@@ -27,7 +28,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID", unique = true)
     public Long getId() {
-        return Id;
+        return id;
     }
 
     @NotNull
@@ -37,20 +38,31 @@ public class Cart {
         return user;
     }
 
-
-    private void setOrder(Order order) {
-        this.order = order;
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "carts")
+    public List<Product> getProductsAddedToCart() {
+        return productsAddedToCart;
     }
 
-    private void setId(Long id) {
-        Id = id;
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ORDER_ID")
+    public Order getOrder() {
+        return order;
     }
 
-    private void setUser(User user) {
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
         this.user = user;
     }
 
-    private void setProductsAddedToCart(List<Product> productsAddedToCart) {
+    public void setProductsAddedToCart(List<Product> productsAddedToCart) {
         this.productsAddedToCart = productsAddedToCart;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
     }
 }
