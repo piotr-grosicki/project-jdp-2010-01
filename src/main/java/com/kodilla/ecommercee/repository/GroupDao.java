83870eb30899d9d1c5f8b5fb9e87622df2d7a500
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-11-08 12:33:00 +0100

    creating classes

---
+++ src/main/java/com/kodilla/ecommercee/repository/GroupDao.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Group;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface GroupDao extends CrudRepository <Group, Long> {
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Optional<Group> findById(Long groupId);
+
+    @Override
+    Group save(Group Group);
+
+    @Override
+    void deleteById(Long groupId);
+
+}
---
+++ src/test/java/com/kodilla/ecommercee/group/dao/GroupDaoTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.ecommercee.group.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupDaoTestSuite {
+
+    @Test
+    public void createGroup() {
+
+    }
+
+    @Test
+    public void saveProductListTest() {
+
+    }
+
+    @Test
+    public void deleteGroupTest() {
+
+    }
+
+
+}
