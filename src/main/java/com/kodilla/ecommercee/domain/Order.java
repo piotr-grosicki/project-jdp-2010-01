bebd36697db3ad5760463a6593ddc22c8d0d14c0
andzia0520 <ankadaniluk@gmail.com>
2020-11-09 22:48:43 +0100

    conflict resolving

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -28,10 +28,6 @@ public class Order {
     public Order() {
     }
 
-    public Order(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @NotNull
