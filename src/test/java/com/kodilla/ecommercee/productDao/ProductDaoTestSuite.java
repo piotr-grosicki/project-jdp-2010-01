bb75e0f3b158c3dff9c61e6f3f3ccc0754c2a7bd
IngaPanczyk <ingawaaa@gmail.com>
2020-11-09 09:07:11 +0100

    zmianan nazw testów, poprawki z review

--- src/test/java/com/kodilla/ecommercee/productDao/ProductDaoTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/productDao/ProductDaoTestSuite.java
@@ -50,8 +50,10 @@ public class ProductDaoTestSuite {
     @Test
     public void saveProductOnDatabaseWithGroupTestSuite() {
         //Given
-        Group group = new Group("Szlifierki Kątowe Bosch");
+        Group group = new Group("Test Product");
+
         Product product = new Product("Test Product");
+        group.getProductList().add(product);
         product.setGroup(group);
         productDao.save(product);
 
@@ -105,9 +107,9 @@ public class ProductDaoTestSuite {
         long cart1Id = cart1.getId();
 
         Optional<Cart> cartResult= cartDao.findById(cart1Id);
-        Optional<Product> product1Result= productDao.findById(prduct1Id);
-        Optional<Product> product2Result= productDao.findById(prduct2Id);
-        Optional<Product> product3Result= productDao.findById(prduct3Id);
+        Optional<Product> product1Result = productDao.findById(prduct1Id);
+        Optional<Product> product2Result = productDao.findById(prduct2Id);
+        Optional<Product> product3Result = productDao.findById(prduct3Id);
 
         //Then
         Assert.assertTrue(cartResult.isPresent());
