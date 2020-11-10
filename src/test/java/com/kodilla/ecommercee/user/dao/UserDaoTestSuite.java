b1e75ec5bb9eeebb4553a4731da0ecdadc59ea1d
dud3k88 <mateusz.dudek88@gmail.com>
2020-11-10 21:25:01 +0100

    poprawa nazw testów v_2

--- src/test/java/com/kodilla/ecommercee/user/dao/UserDaoTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/user/dao/UserDaoTestSuite.java
@@ -10,14 +10,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
 public class UserDaoTestSuite {
     @Autowired
     UserDao userDao;
